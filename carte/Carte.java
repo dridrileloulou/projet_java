68f3e1687bf5eec86b4edda1e1adf9482c8aa978
CAZAUX Aymeric <Aymeric.Cazaux@bordeaux-inp.fr>
2025-11-25 18:18:27 +0100

    avancée sur carte et section

--- carte/Carte.java
+++ carte/Carte.java
@@ -1,2 +1,28 @@
-package carte
+package carte;
 
+public class Carte{
+
+    private int valeur;
+    private int type;
+    private String couleur;
+
+    public Carte(){
+        this.valeur = 0;
+        this.type = 0;
+        this.couleur = " ";
+    }
+
+    public Carte(int valeur, int type, String couleur){
+        this.valeur = valeur;
+        this.type = type;
+        this.couleur = couleur;
+    }
+
+    public int get_valeur(){
+        return valeur;
+    }
+
+    public String get_couleur(){
+        return couleur;
+    }
+}
---
+++ section/Section.java
@@ -0,0 +1,27 @@
+package section;
+import carte.Carte;
+import joueur.Joueur;
+
+public class Section{
+    Bool borne;
+    Carte[] = cartes_j1;
+    Carte[] = cartes_j2;
+
+    public Section(){
+        this.borne = true;
+        this.cartes_j1 = Carte[1];
+        this.cartes_j2 = Carte[1];
+    }
+
+    public Section(Bool borne, Carte[] cartes_j1, Carte[] cartes_j2){
+        this.borne = borne;
+        this.cartes_j1 = cartes_j1;
+        this.cartes_j2 = cartes_j2;
+    }
+
+    public void ajouter_carte(Carte c, Joueur j){
+        if (j.getNum_joueur() == 1){
+            for i in 
+        }
+    }
+}
