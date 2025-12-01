975ed6976778a67c2f92f445ece67e1fd357a2cd
CAZAUX Aymeric <Aymeric.Cazaux@bordeaux-inp.fr>
2025-12-01 14:34:24 +0100

    avancées sur section

--- section/Section.java
+++ section/Section.java
@@ -21,7 +21,36 @@ public class Section{
 
     public void ajouter_carte(Carte c, Joueur j){
         if (j.getNum_joueur() == 1){
-            for i in 
+            for (int i = 0; i<cartes_j1.length; i++){
+                if (cartes_j1[i] == null){
+                    cartes_j1[i] = c;
+                    return;
+                }
+            }
+            System.out.println("Plus de place");
+        }
+        else if (j.getNum_joueur() == 2){
+            for (int i = 0; i<cartes_j2.length; i++){
+                if (cartes_j2[i] == null){
+                    cartes_j2[i] = c;
+                    return;
+                }
+            }
+            System.out.println("Plus de place");
+        }
+        else {
+            System.out.println("Joueur inconnu");
+        }
+    }
+
+    public int calculer_force(){
+        int[] v1 = new int[3];
+        String[] c1 = new String[3];
+        for (int i=0; i<cartes_j1.length; i++){
+            if (cartes_j1[i] == null){
+                return 0;
+            }
+            v1[i] = cartes_j1[i].get_valeur();
         }
     }
 }
